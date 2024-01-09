eade4d522d14a1ec6e25988d88c9bde05fbb4846
thanhdi721 <thanhdi721@gmail.com>
2024-01-09 10:48:08 +0700

    updata login

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,144 @@
 import React from "react";
+import logo from "../../assets/images/logo_black.png";
 import imagesTravel from "../../assets/images/imgtravel.png";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./app.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 const HomePage = () => {
+  const navigate = useNavigate();
+  const handleNavigateLogin = () => {
+    navigate("/login");
+  };
+  // const user = useSelector((state) => state.user);
   return (
     <>
+      <header className="header navbar-area">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-12">
+              <div className="nav-inner">
+                <nav className="navbar navbar-expand-lg">
+                  <Link className="navbar-brand" href="/">
+                    <img src={logo} alt="Logo" />
+                  </Link>
+                  <button
+                    className="navbar-toggler mobile-menu-btn"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span className="toggler-icon"></span>
+                    <span className="toggler-icon"></span>
+                    <span className="toggler-icon"></span>
+                  </button>
+                  <div
+                    className="collapse navbar-collapse sub-menu-bar"
+                    id="navbarSupportedContent"
+                  >
+                    {/* <ul id="nav" className="navbar-nav ms-auto">
+                      <li className="nav-item">
+                        <a
+                          href="#home"
+                          className="page-scroll active"
+                          aria-label="Toggle navigation"
+                        >
+                          Home
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="#features"
+                          className="page-scroll"
+                          aria-label="Toggle navigation"
+                        >
+                          Features
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          aria-label="Toggle navigation"
+                          href="#overview"
+                          className="page-scroll"
+                        >
+                          Overview
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="#pricing"
+                          className="page-scroll"
+                          aria-label="Toggle navigation"
+                        >
+                          Pricing
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="#Team"
+                          className="page-scroll"
+                          aria-label="Toggle navigation"
+                        >
+                          Team
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="#Blog"
+                          className="page-scroll"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#submenu-1-4"
+                          aria-controls="navbarSupportedContent"
+                          aria-expanded="false"
+                          aria-label="Toggle navigation"
+                        >
+                          Blog
+                        </a>
+                        <ul className="sub-menu collapse" id="submenu-1-4">
+                          <li className="nav-item">
+                            <div>Blog Grid Sidebar</div>
+                          </li>
+                          <li className="nav-item">
+                            <div>Blog Single</div>
+                          </li>
+                          <li className="nav-item">
+                            <div>Blog Single Sibebar</div>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="#Contact"
+                          className="page-scroll"
+                          aria-label="Toggle navigation"
+                        >
+                          Contact
+                        </a>
+                      </li>
+                    </ul> */}
+                  </div>
+                  {/* {user?.name ? (
+                    <div className="button add-list-button">
+                      <div className="btn" onClick={handleNavigateLogin}>
+                        {user.name}
+                      </div>
+                    </div>
+                  ) : ( */}
+                  <div className="button add-list-button">
+                    <div className="btn" onClick={handleNavigateLogin}>
+                      Tài Khoản
+                    </div>
+                  </div>
+                  {/* )} */}
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
       <section id="home" className="hero-area">
         <div className="container">
           <div className="row align-items-center">
@@ -386,6 +520,157 @@ const HomePage = () => {
           </div>
         </div>
       </section>
+      <footer className="footer">
+        <div className="footer-top">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4 col-md-4 col-12">
+                <div className="single-footer f-about">
+                  <div className="logo">
+                    <a href="index.html">
+                      <img src={logo} alt="#" />
+                    </a>
+                  </div>
+                  <span>
+                    Making the world a better place through constructing elegant
+                    hierarchies.
+                  </span>
+                  <ul className="social">
+                    <li>
+                      <div>
+                        <i className="lni lni-facebook-filled"></i>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <i className="lni lni-twitter-original"></i>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <i className="lni lni-instagram"></i>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <i className="lni lni-linkedin-original"></i>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <i className="lni lni-youtube"></i>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <i className="lni lni-pinterest"></i>
+                      </div>
+                    </li>
+                  </ul>
+                  <span className="copyright-text">
+                    Designed and Developed by
+                    <div rel="nofollow" target="_blank">
+                      UIdeck
+                    </div>
+                  </span>
+                </div>
+              </div>
+              <div className="col-lg-8 col-md-8 col-12">
+                <div className="row">
+                  <div className="col-lg-3 col-md-6 col-12">
+                    <div className="single-footer f-link">
+                      <h3>Solutions</h3>
+                      <ul>
+                        <li>
+                          <div>Marketing</div>
+                        </li>
+                        <li>
+                          <div>Analytics</div>
+                        </li>
+                        <li>
+                          <div>Commerce</div>
+                        </li>
+                        <li>
+                          <div>Insights</div>
+                        </li>
+                        <li>
+                          <div>Promotion</div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="col-lg-3 col-md-6 col-12">
+                    <div className="single-footer f-link">
+                      <h3>Support</h3>
+                      <ul>
+                        <li>
+                          <div>Pricing</div>
+                        </li>
+                        <li>
+                          <div>Documentation</div>
+                        </li>
+                        <li>
+                          <div>Guides</div>
+                        </li>
+                        <li>
+                          <div>API Status</div>
+                        </li>
+                        <li>
+                          <div>Live Support</div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="col-lg-3 col-md-6 col-12">
+                    <div className="single-footer f-link">
+                      <h3>Company</h3>
+                      <ul>
+                        <li>
+                          <div>About Us</div>
+                        </li>
+                        <li>
+                          <div>Our Blog</div>
+                        </li>
+                        <li>
+                          <div>Jobs</div>
+                        </li>
+                        <li>
+                          <div>Press</div>
+                        </li>
+                        <li>
+                          <div>Contact Us</div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="col-lg-3 col-md-6 col-12">
+                    <div className="single-footer f-link">
+                      <h3>Legal</h3>
+                      <ul>
+                        <li>
+                          <div>Terms & Conditions</div>
+                        </li>
+                        <li>
+                          <div>Privacy Policy</div>
+                        </li>
+                        <li>
+                          <div>Catering Services</div>
+                        </li>
+                        <li>
+                          <div>Customer Relations</div>
+                        </li>
+                        <li>
+                          <div>Innovation</div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
